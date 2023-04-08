ab70eb366fa2fe5acc1ff782aaca2b71dbc9640c
pannorris <jakub.stefankiewicz@wp.pl>
2023-04-08 16:37:26 +0200

    wyswietlanie zadan

---
+++ workshop/js/app.js
@@ -0,0 +1,107 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+const apikey="9e0d6bfd-4478-4627-88b7-6546a50038ff";
+const apihost = 'https://todo-api.coderslab.pl';
+
+function apiListTasks() {
+    return fetch(
+        apihost + '/api/tasks',
+        {
+            headers: { Authorization: apikey }
+        }
+    ).then(
+        function(resp) {
+            if(!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        }
+    )
+}
+
+    function renderTask(taskId, title, description, status) {
+
+        const section = document.createElement("section");
+        section.className = 'card mt-5 shadow-sm';
+        document.querySelector('main').appendChild(section);
+
+        const headerDiv = document.createElement('div');
+        headerDiv.className = 'card-header d-flex justify-content-between align-items-center';
+        section.appendChild(headerDiv);
+
+        const headerLeftDiv = document.createElement('div');
+        headerDiv.appendChild(headerLeftDiv);
+
+        const h5 = document.createElement('h5');
+        h5.innerText = title;
+        headerLeftDiv.appendChild(h5);
+
+        const h6 = document.createElement('h6');
+        h6.className = 'card-subtitle text-muted';
+        h6.innerText = description;
+        headerLeftDiv.appendChild(h6);
+
+        const headerRightDiv = document.createElement('div');
+        headerDiv.appendChild(headerRightDiv);
+
+        if(status === 'open') {
+            const finishButton = document.createElement('button');
+            finishButton.className = 'btn btn-dark btn-sm js-task-open-only';
+            finishButton.innerText = 'Finish';
+            headerRightDiv.appendChild(finishButton);
+        }
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'btn btn-outline-danger btn-sm ml-2';
+        deleteButton.innerText = 'Delete';
+        headerRightDiv.appendChild(deleteButton);
+
+        const ul=document.createElement("ul");
+        ul.className="list-group list-group-flush";
+        section.appendChild(ul);
+
+        const li=document.createElement("li");
+        li.className="list-group-item d-flex justify-content-between align-items-center";
+        ul.appendChild(li);
+
+    }
+
+
+
+apiListTasks().then(function (response){
+    response.data.forEach(function (task){
+        renderTask(task.id, task.title, task.description, task.status);
+    })
+})
+
+
+
+function apiCreateTask(title, description) {
+    return fetch(
+        apihost + '/api/tasks',
+        {
+            method: 'POST',
+            headers: {
+                'Authorization': apikey,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title: title, description: description, status: 'open' })
+        }
+    ).then(
+        function(resp) {
+            if(!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        }
+    )
+}
+
+// apiCreateTask(`zadanie`, `opis`).then(
+//     function (response){
+//         console.log(response);
+//     }
+// );
+
+
+})
